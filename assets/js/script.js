fb85c45a951769c690566fdc9867c63f31261e47
Usman Alvi <usmanalvi537@gmail.com>
2024-10-28 22:56:36 -0700

    files added

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+$(".testi-clients").slick({
+  dots: true,
+  arrow: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current === end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("counter1", 0, 1287, 3000);
+  counter("counter2", 100, 1387, 3000);
+  counter("counter3", 1200, 2387, 3000);
+  counter("counter4", 0, 1487, 3000);
+});
